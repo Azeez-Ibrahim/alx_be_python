93720dfae6dc9687f2ff518c44fbd7e8c6a0d953
Azeez-Ibrahim <azeez2910@outlook.com>
2024-07-05 07:13:10 +0100

    reformat code

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -5,31 +5,33 @@ def display_menu():
     print("3. View List")
     print("4. Exit")
 
+
 def main():
     shopping_list = []
     while True:
         display_menu()
         choice = input("Enter your choice: ")
 
-        if choice == '1':
+        if choice == "1":
             # Prompt for and add an item
             add_list = input("Enter the item to add: ")
             shopping_list.append(add_list)
             pass
-        elif choice == '2':
+        elif choice == "2":
             # Prompt for and remove an item
             remove_list = input("Enter the item to remove: ")
             shopping_list.remove(remove_list)
             pass
-        elif choice == '3':
+        elif choice == "3":
             # Display the shopping list
             print(shopping_list)
             pass
-        elif choice == '4':
+        elif choice == "4":
             print("Goodbye!")
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()

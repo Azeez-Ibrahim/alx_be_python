91ae28056ceee9f59461bd334909fa662ec21117
Azeez-Ibrahim <azeez2910@outlook.com>
2024-07-05 07:05:25 +0100

    modify input statement

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,12 +13,12 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            add_list = input("Add to list: ")
+            add_list = input("Enter the item to add: ")
             shopping_list.append(add_list)
             pass
         elif choice == '2':
             # Prompt for and remove an item
-            remove_list = input("Remove to list: ")
+            remove_list = input("Enter the item to remove: ")
             shopping_list.remove(remove_list)
             pass
         elif choice == '3':

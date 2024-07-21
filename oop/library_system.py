04f4920902f50a1df05ce8b698fd15ba1f871813
Azeez-Ibrahim <azeez2910@outlook.com>
2024-07-21 20:10:02 +0100

    change how list_books method works using __str__

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,6 +18,10 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        """Returns a string representation of the Book."""
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     """
@@ -39,6 +43,10 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        """Returns a string representation of the EBook."""
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 
 class PrintBook(Book):
     """
@@ -59,6 +67,10 @@ class PrintBook(Book):
         """
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        """Returns a string representation of the PrintBook."""
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -85,9 +97,4 @@ class Library:
     def list_books(self):
         """Prints details of each book in the library."""
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
